8bae8ad138f873320e5c1862e736eee4c8b1279f
chenghaopeng <i@chper.cn>
2019-09-20 14:00:33 +0800

    😃 init be and fe

---
+++ be/src/main/java/cn/chper/praintf/PraintfApplication.java
@@ -0,0 +1,13 @@
+package cn.chper.praintf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraintfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraintfApplication.class, args);
+    }
+
+}
