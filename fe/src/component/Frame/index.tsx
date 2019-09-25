757c95b8c4d63d3c48d8e543ac389bd126bb5a7f
chenghaopeng <i@chper.cn>
2019-09-25 19:24:19 +0800

    Sidebar组件改名为HeaderCard，且初步完成

--- fe/src/component/Frame/index.tsx
+++ fe/src/component/Frame/index.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import styles from "./index.module.less";
 import { Row, Col } from "antd";
 
-import Sidebar from "../Sidebar";
+import HeaderCard from "../HeaderCard";
 
 export default function Frame () {
   return (
     <div className={styles.whole}>
       <Row className={styles.container}>
-        <Col xs={{span: 20, offset: 0}} sm={{span: 20, offset: 0}} md={{span: 5, offset: 0}} lg={{span: 6, offset: 0}} className={`${styles.sideBar} ${styles.defaultBox}`}>
-          <Sidebar/>
+        <Col xs={{span: 20, offset: 0}} sm={{span: 20, offset: 0}} md={{span: 6, offset: 0}} className={`${styles.sideBar} ${styles.defaultBox}`}>
+          <HeaderCard/>
         </Col>
-        <Col xs={{span: 20, offset: 0}} sm={{span: 20, offset: 2}} md={{span: 16, offset: 1}} lg={{span: 15, offset: 1}} className={styles.content}>
+        <Col xs={{span: 20, offset: 0}} sm={{span: 20, offset: 2}} md={{span: 15, offset: 1}} className={styles.content}>
           内容
         </Col>
       </Row>
---
+++ fe/src/component/HeaderCard/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styles from "./index.module.less";
+import { Avatar, Tooltip } from "antd";
+import { TiSocialGithub, TiLocation, TiMail, TiMessages, TiBusinessCard} from "react-icons/ti";
+
+import avatar from "../../resource/avatar.png";
+
+const icons = [
+  {
+    component: TiSocialGithub,
+    tooltip: "GitHub",
+    onClick: () => {}
+  },
+  {
+    component: TiMail,
+    tooltip: "邮箱",
+    onClick: () => {}
+  },
+  {
+    component: TiMessages,
+    tooltip: "交流",
+    onClick: () => {}
+  },
+  {
+    component: TiLocation,
+    tooltip: "南京大学软件学院",
+    onClick: () => {}
+  },
+  {
+    component: TiBusinessCard,
+    tooltip: "我",
+    onClick: () => {}
+  },
+];
+
+function Icons() {
+  return icons.map((item) => {
+    return (
+      <Tooltip title={item.tooltip}>
+        <item.component className={styles.icon} onClick={item.onClick}/>
+      </Tooltip>
+    );
+  });
+}
+
+export default function HeaderCard() {
+  return (
+    <div className={styles.whole}>
+      <Avatar src={avatar} style={{height: "8em", width: "8em"}}/>
+      <div className={styles.title}>
+        Chaop
+      </div>
+      <div className={styles.icons}>
+        {Icons()}
+      </div>
+    </div>
+  );
+}
