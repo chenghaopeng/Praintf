4b0671f60287f084aa61142f213a661367468bbc
chenghaopeng <i@chper.cn>
2019-10-03 13:30:27 +0800

    识别仅在Linux系统上加载redux开发工具

--- fe/src/store.tsx
+++ fe/src/store.tsx
@@ -4,9 +4,13 @@ import rootReducer from "./reducer";
 
 const store = createStore(
   rootReducer,
+  (String(navigator.platform).indexOf("Linux") > -1) ? 
   compose(
     applyMiddleware(ReduxThunk),
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  ) :
+  compose(
+    applyMiddleware(ReduxThunk),
   )
 );
 
